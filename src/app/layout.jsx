fdc4e4a953a8e95ae7063ed399675cbd686a45fd
Michael Asare <55958142+United1l@users.noreply.github.com>
2026-04-08 22:12:19 +0000

    Removed the overflow-y-hidden property from the whole site

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scrollbar overflow-y-hidden">
+    <html lang="en" className="scrollbar">
       <body>
         <Nav />
         {children}
